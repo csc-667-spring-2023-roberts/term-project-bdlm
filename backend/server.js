4d7baf8fa45547fcc85bb2475a4f9865f85966e0
Leo Saeteurn <leosaeteurn@gmail.com>
2023-04-22 21:25:46 -0700

    reconfigure server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
-const path = require("path");
+const express = require("express");
 const createError = require("http-errors");
-const cookieParser = require("cookie-parser");
+const path = require("path");
+
 const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+require("dotenv").config();
+
 const homeRoutes = require("./routes/static/home.js");
 const authenticationRoutes = require("./routes/static/authentication.js");
 const gameroomRoutes = require("./routes/static/gameroom.js");
@@ -9,18 +14,13 @@ const tableRoutes = require("./routes/static/table.js");
 const tableroomRoutes = require("./routes/static/tableroom.js");
 const userhomeRoutes = require("./routes/static/userhome.js");
 
-require("dotenv").config();
-
-const express = require("express");
 const app = express();
-const testRoutes = require("./routes/test/index.js");
-
-
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 // if (process.env.NODE_ENV === "development") {
 //     const livereload = require("livereload");
 //     const connectLiveReload = require("connect-livereload");
@@ -35,27 +35,29 @@ app.use(cookieParser());
 
 //     app.use(connectLiveReload());
 // }
+
+const PORT = process.env.PORT || 3000;
+
 app.set("views", path.join(__dirname, "backend", "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
+
 app.use(express.static(path.join(__dirname, "backend", "static")));
 
-const rootRoutes = require("./routes/root");
 
-//app.use("/", rootRoutes);
 app.use("/", homeRoutes);
 app.use("/authentication", authenticationRoutes);
 app.use("/gameroom", gameroomRoutes);
 app.use("/tableroom", tableroomRoutes);
 app.use("/table", tableRoutes);
 app.use("/userhome", userhomeRoutes);
-//app.use("/test", testRoutes);
 
 
-const PORT = process.env.PORT || 3000;
+
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
 
-app.use((request, response, next) => {
+app.use((_request, _response, next) => {
   next(createError(404));
 });
