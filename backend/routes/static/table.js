836034cbf206df6e4fa2f6948a08ba1bd8f62413
Leo Saeteurn <leosaeteurn@gmail.com>
2023-04-22 21:36:36 -0700

    modify files to correct modules

--- backend/routes/static/table.js
+++ backend/routes/static/table.js
@@ -5,7 +5,7 @@ const router = express.Router();
 //need table id separate from other id
 router.get("/:id", (_request, response) => {
   const { id } = request.params;
-  response.render("table", { id, title: "GAME ROOM PAGE" });
+  response.render("table", { id, title: "TABLE PAGE" });
 });
 
 module.exports = router;
