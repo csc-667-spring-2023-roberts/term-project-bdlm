6f9a835a58c842b3ed46b3b39e05cc456f3f29c0
ledenean <dle@sfsu.edu>
2023-04-30 16:23:25 -0700

    changes to user sql queries

--- backend/db/users.js
+++ backend/db/users.js
@@ -1,15 +1,19 @@
-const db = require("./connection.js");
-
-const create = (username, email, hash) =>
-  db.one(
-    "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id",
-    [username, email, hash]
-  );
-
-const findByEmail = (email) =>
-  db.one("SELECT * FROM users WHERE email=$1", [email]);
+const db = require("./connection");
+const create = (username, email, password) => db.one(
+    "INSERT INTO user (username, email, password) VALUES ($1, $2, $3) RETURNING id",
+    [username, email, password]
+);
+const findByUsername = (username) => db.one(
+    "SELECT * FROM user WHERE username=$1",
+    [username]
+);
+const findByEmail = (email) => db.one(
+    "SELECT * FROM user WHERE email=$1", 
+    [email]
+);
 
 module.exports = {
-  create,
-  findByEmail,
+    create,
+    findByUsername,
+    findByEmail,
 };
