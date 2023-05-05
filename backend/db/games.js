ad1d06108fef26cdd6964b0a5ea00cf2321ac503
ledenean <dle@sfsu.edu>
2023-05-04 20:56:01 -0700

    made changes to game files

--- backend/db/games.js
+++ backend/db/games.js
@@ -2,33 +2,39 @@ const db = require("./connection");
 
 /* ---Denean's logic for this particular file---
 1. Create Room -> referring the rooms that contain several tables to join, based on the betting range
-2. Create Game Table 
-3. List available game tables
-4. Add players -> player joins a table to play
-5. Update table -> for updating the playerCount
-6. Create player cards
-7. Draw cards from pile (???) 
-8. Update player cards -> as a result of drawing a card
-9. 
+2. Check Rooms -> one room per matchType (based on wireframes, there should only be 4 match types)
+3. Create Game Table 
+4. List available game tables
+5. Add players -> player joins a table to play
+6. Update table -> for updating the playerCount
+7. Create player cards
+8. Draw cards from pile (???) 
+9. Update player cards -> as a result of drawing a card
+ 
 ----- IN PROGRESS -----
 */
 
 const createRoom = (matchType) => db.one(
-  "INSERT INTO gameRoom (matchType) VALUES ($1)", 
+  "INSERT INTO gameRoom (matchType) VALUES ($1) RETURNING id", 
   [matchType]
 );
+const checkRooms = (matchType) => db.any(
+  "SELECT COUNT(*) FROM gameRoom r WHERE r.matchType=$1",
+  [matchType]
+)
 const createGameTable = (roomId, playerCount) => db.one(
   "INSERT INTO gameTable (roomId, playerCount) VALUES ($1, $2)", 
   [roomId, playerCount]
 );
-const getTableList = async (userId) => db.any(
-  "SELECT t.id FROM gameTable t, players p WHERE t.id = p.tableId AND p.id != $1 AND (SELECT COUNT(*) FROM players p WHERE p.tableId=t.id) < 4",
+// Gets tables that has not met max capacity of players
+const getTableList = async (roomId) => db.any(
+  "SELECT t.id FROM gameTable t WHERE t.roomId=$1 AND t.playerCount < 4",
   [userId]
 );
 const joinTable = async(tableId, userId) => { 
-  const { playercount } = await db.one("SELECT playerCount FROM gameTable tWHERE t.id=$1", [tableId]);
+  const { playerCount } = await db.one("SELECT playerCount FROM gameTable tWHERE t.id=$1", [tableId]);
 
-  await db.none("INSERT INTO players (tableId, userId)", [tableId, userId]);
+  await db.none("INSERT INTO players (tableId, userId, tableOrder)", [tableId, userId, playerCount]);
 };
 const updateTable = (playerCount, tableId) => db.none(
   "UPDATE gameTable SET playerCount=$1 WHERE tableId=$2",
@@ -42,12 +48,14 @@ const updatePlayerCards = (cardId, cardOrder, userId) => db.none(
   "UPDATE gameCards SET cardId=$1 AND cardOrder=$2 WHERE playerId=$3",
   [cardId, cardOrder, userId]
 );
+// Gets list of players inside the table
 const getPlayersList = (tableId) => db.any (
   "SELECT id, username FROM user u, players p WHERE p.tableId=$1 AND p.userId=u.id",
   [tableId]
 );
 module.exports = {
   createRoom,
+  checkRooms,
   createGameTable,
   getTableList,
   joinTable,
--- backend/routes/static/games.js
+++ backend/routes/static/games.js
@@ -4,6 +4,7 @@ const { isAuthenticated } = require("../../middleware/is-authenticated.js");
 
 const router = express.Router();
 
+<<<<<<< HEAD
 router.post("/create", async (request, response) => {
   const { id: userId } = request.session.user;
 
@@ -18,6 +19,44 @@ router.post("/create", async (request, response) => {
 router.get("/:id", (request, response) => {
   const { id } = request.params;
   response.render("games", { id, title: "BDLM Term Project" });
+=======
+router.get('/:matchType', async(request, response) => {
+    const { matchType } = request.params;
+    try {
+        const { check } = await Games.checkRooms(matchType);
+        if (check < 1) {
+            console.log("Room does not exist. Creating new room .... ")
+            const { roomId } = Games.createRoom(matchType);
+            response.redirect(`/games/${roomId}`);
+        }
+        else{
+            console.log("Room already exists");
+            const { tableList } = Games.getTableList(roomId);
+            response.redirect(`/gameroom/${tableList}`);
+        }
+    } catch( error ){
+        console.log(error);
+        response.redirect("/lobby");
+    }
+});
+router.post("/createTable", async (request, response) => {
+    const { id: userId } = request.session.user;
+
+    try {
+        const { id: tableId} = await Games.createGameTable(userId);
+        response,redirect(`/games/${tableId}`);
+    } catch (error){
+        console.log({error});
+        response.redirect("/lobby");
+    }
+}); 
+
+
+
+router.get("/:id", (request, response) => {
+    const { id } = request.params;
+    response.render("games", { id, title: "Game Page"});
+>>>>>>> c9458ea (creating game room from routes)
 });
 
 module.exports = router;
