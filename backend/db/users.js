582e0d89ca307e8390db4854410b4af13ab1cbc0
Salvatim007 <martysaljhost@gmail.com>
2023-05-04 18:16:24 -0700

    changes

--- backend/db/users.js
+++ backend/db/users.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-const db = require("./connection.js");
-
-const create = (firstname, lastname, username, email, hash) =>
-  db.one(
-    "INSERT INTO users (firstname, lastname, username, email, password) VALUES ($1, $2, $3, $4, $5) RETURNING id",
-    [firstname, lastname, username, email, hash]
-  );
-
-const findByEmail = (email) =>
-  db.one("SELECT * FROM users WHERE email=$1", [email]);
-=======
 const db = require("./connection");
 const create = (username, email, password, firstName, lastName) => db.one(
     "INSERT INTO user (username, email, password, firstName, lastName) VALUES ($1, $2, $3, $4, $5) RETURNING id",
@@ -23,7 +11,6 @@ const findByEmail = (email) => db.one(
     "SELECT * FROM user WHERE email=$1", 
     [email]
 );
->>>>>>> db1486df95c3516bf7d16db78dae2bdc0904ec68
 
 module.exports = {
     create,
