940155a79fdb4c932b0ac29a2d2265e77d415205
Burned357Waffles <bwatanabe@sfsu.edu>
2023-05-07 23:46:19 -0700

    minor changes

--- backend/routes/static/lobby.js
+++ backend/routes/static/lobby.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const { isAuthenticated } = require("../../middleware/is-authenticated.js");
+//const { isAuthenticated } = require("../../middleware/is-authenticated.js");
 
 const router = express.Router();
 //
-router.get("/lobby", (_request, response) => {
-  response.render("lobby", { title: "LOBBY" });
+router.get("/lobby", (rep, res) => {
+  res.render("lobby", { title: "LOBBY" });
 });
 
 module.exports = router;
