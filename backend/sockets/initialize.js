7c08506e567398077d57bd34947de444f5663524
ledenean <dle@sfsu.edu>
2023-05-10 16:07:42 -0700

    fixed error when loading initialize.js

--- backend/sockets/initialize.js
+++ backend/sockets/initialize.js
@@ -9,19 +9,32 @@ const initSockets = (app, sessionMiddleware) => {
 
   io.engine.use(sessionMiddleware);
 
-  io.on("connection", (socket) => {
-    const { id: user_id } = socket.request.session.user;
+  io.on("connection", async (socket) => {
+    // const {id: user_id } = await socket.request.session.user;
     const { id: socket_id } = socket;
+    console.log(socket.request.session);
 
-    let table_id = socket.handshake.query.path.substring(1);
+    // let table_id = socket.handshake.query.path.substring(1);
+    // if (table_id === "lobby") {
+    //   table_id = 0;
+    // }
+    // else{
+    //   table_id = parseInt(table_id.substring(table_id.lastIndexOf("/") + 1));
+    // }
+    // Sockets.store(user_id, socket_id, table_id);
+    if (socket.request.session.user !== undefined) {
+      const { id: user_id } = socket.request.session.user;
+      let table_id = socket.handshake.query.path.substring(1);
 
-    if (table_id === "lobby") {
-      table_id = 0;
-    } else {
-      table_id = parseInt(table_id.substring(table_id.lastIndexOf("/") + 1));
+      if (table_id === "lobby") {
+        table_id = 0;
+      } else {
+        table_id = parseInt(table_id.substring(table_id.lastIndexOf("/") + 1));
+      }
+      // Sockets.store(user_id, socket_id, table_id);
     }
 
-    Sockets.store(user_id, socket_id, table_id);
+
 
     socket.on("disconnect", () => {
       Sockets.remove(socket_id);
