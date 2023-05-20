2fafb8c18c8fe819f3f85743ff63eef4dff94ef6
Burned357Waffles <bwatanabe@sfsu.edu>
2023-05-19 17:10:11 -0700

    fixed repeat joins

--- backend/db/games/join.js
+++ backend/db/games/join.js
@@ -1,18 +1,32 @@
 const db = require("../connection.js");
 
 const join = async (table_id, user_id, is_current = false) => {
-  await db.none(
-    `INSERT INTO players 
-    (user_id, table_id, current, "table_order", bet) 
-    SELECT $1, $2, $3, count(*) + 1, 0 FROM players 
-    WHERE table_id=$2 `,
-    [user_id, table_id, is_current]
-  );
-  await db.none(
-    `UPDATE gametable
-    SET player_count = player_count + 1
-    WHERE id=$1`,
-    [table_id]
-  );
+  try {
+    const player = await db.one(
+      "SELECT user_id FROM players WHERE table_id=$1 AND user_id=$2",
+      [table_id, user_id]
+    );
+  } catch (error) {
+    console.log("Player not in table.");
+
+    await db.none(
+      `INSERT INTO players 
+      (user_id, table_id, current, "table_order", bet) 
+      SELECT $1, $2, $3, count(*) + 1, 0 FROM players 
+      WHERE table_id=$2 `,
+      [user_id, table_id, is_current]
+    );
+    await db.none(
+      `UPDATE gametable
+      SET player_count = player_count + 1
+      WHERE id=$1`,
+      [table_id]
+    );
+
+    return;
+  }
+
+  console.log("You are already in this game");
+  return;
 };
 module.exports = { join };
